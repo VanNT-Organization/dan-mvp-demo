24d06f64cb90575865969ca6f700e2d1503a2cd2
Khiemvx <khiemvu.freelan@gmail.com>
2023-01-13 23:23:09 +0700

    fix bug not register dio

--- lib/src/di/sign_in_di.dart
+++ lib/src/di/sign_in_di.dart
@@ -4,9 +4,12 @@ import 'package:mvp/src/data/service/sign_in_service.dart';
 import 'package:mvp/src/data/sources/remote/sign_in_remote_data_source.dart';
 import 'package:mvp/src/domain/usecase/sign_in/sign_in_usecase.dart';
 import 'package:mvp/src/presentation/cubit/sign_in/sign_in_cubit.dart';
+import 'package:dio/dio.dart';
 
 void registerDI() {
   final sl = GetIt.instance;
+  sl.registerLazySingleton<Dio>(
+          () => setupDio(""), instanceName: 'xx');
   // Services
   sl.registerLazySingleton<SignInService>(() => SignInService(
       sl.get(),
@@ -29,4 +32,17 @@ void registerDI() {
 
   //bloc
   sl.registerFactory(() => SignInCubit(signInUseCase: sl.get()));
+}
+
+Dio setupDio(String baseUrl) {
+  final dio = Dio();
+  dio.options.baseUrl = baseUrl;
+  dio.options.connectTimeout = 90000;
+  dio.options.receiveTimeout = 90000;
+  dio.options.sendTimeout = 90000;
+  dio.options.contentType = 'application/json';
+  dio.options.headers.putIfAbsent('lang', () =>'en');
+  dio.options.headers.putIfAbsent('Accept', () => 'application/json');
+
+  return dio;
 }
